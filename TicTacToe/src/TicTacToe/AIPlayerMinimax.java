f414722abf4cee60ebc410e5213a24f799558bfd
peter <peter@BILLY>
2024-12-20 23:18:03 +0700

    Added header

--- TicTacToe/src/TicTacToe/AIPlayerMinimax.java
+++ TicTacToe/src/TicTacToe/AIPlayerMinimax.java
@@ -1,3 +1,10 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #17
+ * 1 - 5026231138 - Peter Christian Erastus
+ */
 package TicTacToe;
 
 import java.util.*;
